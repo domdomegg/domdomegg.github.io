c66d0ecaf5e40a72c461570367a36dcce5c8e3cd
Adam Jones <domdomegg+git@gmail.com>
2023-04-10 18:55:09 +0100

    Add /feedback redirect

---
+++ pages/feedback.tsx
@@ -0,0 +1,8 @@
+const FeedbackPage = () => {
+  if (typeof window !== 'undefined') {
+    window.location.replace('https://docs.google.com/forms/d/e/1FAIpQLSdL0wKeFtzQpcpcyPp25x3vKn8x9WoLS-qbQ97hxjHOsll9EA/viewform');
+  }
+  return <div />;
+};
+
+export default FeedbackPage;
