5674406bac711759a2fdcb15ab6888f1c4f19272
Adam Jones <domdomegg+git@gmail.com>
2024-01-16 00:32:53 +0000

    Update publishing pipeline

--- next.config.js
+++ next.config.js
@@ -16,6 +16,8 @@ module.exports = async () => {
 
   /** @type {import('next').NextConfig} */
   const nextConfig = {
+    output: 'export',
+    distDir: 'dist',
     reactStrictMode: true,
     images: {
       unoptimized: true,
