d280586c18b50154c1063127320685883a7b6fe9
Adam Jones <domdomegg+git@gmail.com>
2024-04-30 01:05:51 +0100

    Upload next vs nginx benchmarking

---
+++ components/BarChart.tsx
@@ -0,0 +1,30 @@
+import { Chart, ChartWrapperOptions } from 'react-google-charts';
+
+const BarChart: React.FC<{ data: (string | number)[][], options?: ChartWrapperOptions['options'] }> = ({ data, options }) => {
+  return (
+    <div className="-mt-8 mb-12">
+      <Chart
+        chartType="BarChart"
+        data={data}
+        options={{
+          legend: {
+            position: 'none',
+          },
+          hAxis: {
+            title: data[0][1] as string,
+            ticks: [],
+          },
+          annotations: {
+            alwaysOutside: true,
+          },
+          backgroundColor: 'transparent',
+          focusTarget: 'category',
+          colors: ['#ff5f66'],
+          ...options,
+        }}
+      />
+    </div>
+  );
+};
+
+export default BarChart;
