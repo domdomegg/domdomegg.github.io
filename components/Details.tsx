c3b945232efc2c61e5fee2c91d401b79f32bcb8d
Adam Jones <domdomegg+git@gmail.com>
2025-02-04 20:22:53 +0000

    components(Details): Use native <details> and <summary> elements
    This allows them to work without JavaScript, and makes the page more Cmd+F, LLM and accessibility friendly.

--- components/Details.tsx
+++ components/Details.tsx
@@ -1,39 +1,17 @@
-import clsx from 'clsx';
-import { useEffect, useState } from 'react';
-
 const Details: React.FC<React.PropsWithChildren<{ title: string }>> = ({ title, children }) => {
-  const [isOpen, setOpen] = useState(true);
-  const toggleOpen = () => setOpen((s) => !s);
-
-  useEffect(() => {
-    if (typeof window !== undefined) {
-      setOpen(false);
-    }
-  }, []);
-
   return (
-    <div
+    <details
       className="my-4 px-4 bg-gray-100 border border-gray-200"
     >
-      <div
-        role="button"
-        tabIndex={0}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter' || e.key === ' ') {
-            toggleOpen();
-            e.preventDefault();
-          }
-        }}
-        onClick={toggleOpen}
-        aria-expanded={isOpen}
-        className="px-4 py-4 -mx-4"
+      <summary
+        className="px-4 py-4 -mx-4 cursor-pointer marker:text-gray-400 marker:scale-75"
       >
-        <span className={clsx('text-gray-400 inline-block transition-all select-none scale-75', { 'rotate-90': isOpen })}>▶</span>
-        {' '}
+        <span className='pl-1.5'>
         {title}
-      </div>
-      {isOpen ? <div className="-mt-4 mb-4">{children}</div> : null}
-    </div>
+        </span>
+      </summary>
+      <div className="-mt-4 mb-4">{children}</div>
+    </details>
   );
 };
 
