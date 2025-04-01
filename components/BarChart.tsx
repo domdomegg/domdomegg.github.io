856de2246c80aa2afd9315beaa5be13b3f7e22ac
Adam Jones <domdomegg+git@gmail.com>
2025-04-01 16:15:19 +0100

    blog: Add article on running LLMs locally

--- components/BarChart.tsx
+++ components/BarChart.tsx
@@ -2,7 +2,7 @@ import {Chart, type ChartWrapperOptions} from 'react-google-charts';
 
 const BarChart: React.FC<{data: (string | number)[][]; options?: ChartWrapperOptions['options']}> = ({data, options}) => {
 	return (
-		<div className='-mt-8 mb-12'>
+		<div className='-mt-4 mb-12'>
 			<Chart
 				chartType='BarChart'
 				data={data}
@@ -20,6 +20,10 @@ const BarChart: React.FC<{data: (string | number)[][]; options?: ChartWrapperOpt
 					backgroundColor: 'transparent',
 					focusTarget: 'category',
 					colors: ['#ff5f66'],
+					height: Math.max(data.length * 30, 200),
+					chartArea: {
+						height: '80%',
+					},
 					...options,
 				}}
 			/>
